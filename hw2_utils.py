166e66f5227793a953bff440912632c76f4e2ccd
JakeVestal <jmvestal@gmail.com>
2021-03-23 18:18:14 -0400

    commit

--- hw2_utils.py
+++ hw2_utils.py
@@ -22,3 +22,34 @@ def fetch_usdt_rates(YYYY):
     df.Date = pd.to_datetime(df.Date)
 
     return df
+
+def Y_m_d_to_unix_str(ymd_str):
+    return str(int(time.mktime(pd.to_datetime(ymd_str).date().timetuple())))
+
+def fetch_GSPC_data(start_date, end_date):
+    # Requests the USDT's daily yield data for a given year. Results are
+    #   returned as a DataFrame object with the 'Date' column formatted as a
+    #   pandas datetime type.
+
+    URL = 'https://finance.yahoo.com/quote/%5EGSPC/history?' + \
+            'period1=' + Y_m_d_to_unix_str(start_date) + \
+            '&period2=' + Y_m_d_to_unix_str(end_date) + \
+            '&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'
+
+    gspc_page = requests.get(URL)
+
+    soup = BeautifulSoup(gspc_page.content, 'html.parser')
+
+    table_html = soup.findAll('table', {'data-test': 'historical-prices'})
+
+
+    df = pd.read_html(str(table_html))[0]
+
+    df.drop(df.tail(1).index, inplace=True)
+
+    # see formats here: https://www.w3schools.com/python/python_datetime.asp
+    df.Date = pd.to_datetime(df.Date)
+
+    return df
+
+
